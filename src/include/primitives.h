4db4c242fd40abbddbaa807d13a319c349348e7b
Anders Gidenstam <anders@gidenstam.org>
2016-03-31 13:39:39 +0200

    Added the selected license: GPL v3+ and for selected parts LGPL v3+.

--- src/include/primitives.h
+++ src/include/primitives.h
@@ -1,5 +1,19 @@
 // Minimal defines for primitives for the EXCESS experiment framework.
-//   Copyright (C) 2014  Anders Gidenstam
+// Copyright (C) 2014  Anders Gidenstam
+//
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef _PRIMITIVES_H
 #define _PRIMITIVES_H
